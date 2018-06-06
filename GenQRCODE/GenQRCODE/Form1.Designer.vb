d8f79fd1391ba70400f333f4f3397b100ca174c6
Parinya <parinyat0926@gmail.com>
2018-06-06 10:48:13 +0700

    Initial commit

---
+++ GenQRCODE/GenQRCODE/Form1.Designer.vb
@@ -0,0 +1,58 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "QRCODE"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Timer2
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(365, 136)
+        Me.Name = "Form1"
+        Me.Text = "QR"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+
+End Class
---
+++ GenQRCODE/GenQRCODE/Form1.vb
@@ -0,0 +1,73 @@
+﻿Imports System.Text
+Imports System.IO
+Imports ZXing
+Imports System.Drawing.Imaging
+Imports ZXing.QrCode
+Public Class Form1
+  
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Interval = 10000
+        Timer1.Start()
+        'Dim encoder As New ZXing.BarcodeWriter
+        'encoder.Format = BarcodeFormat.QR_CODE
+        'encoder.Options.Height = 50
+        'encoder.Options.Width = 50
+        'Dim BimMap = encoder.Write("123456789")
+        'Dim g As Graphics = Graphics.FromImage(BimMap)
+        'BimMap.Save("D:\111.png", System.Drawing.Imaging.ImageFormat.Png)
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim i As Integer = 0
+        Dim stCode As String
+        ' Dim directory = "D:\GenCode"
+        Dim directory = "\\192.168.0.5\DataAX\QRCode"
+        For Each filename As String In IO.Directory.GetFiles(directory, "*", IO.SearchOption.AllDirectories)
+            Dim fname As String = IO.Path.GetExtension(filename)
+            If fname = ".XML" Or fname = ".xml" Then
+                Dim ds As DataSet
+                ds = New DataSet
+                ds.ReadXml(filename)
+                ' MsgBox(ds.Tables(0).Rows(0)(0).ToString)
+                Dim qrcode As New BarcodeFormat
+                stCode = ds.Tables(0).Rows(0)(0).ToString
+                Dim encoder As New ZXing.BarcodeWriter
+                encoder.Format = BarcodeFormat.QR_CODE
+                encoder.Options.Height = 100
+                encoder.Options.Width = 100
+                encoder.Options.Margin = 0
+                Dim BimMap = encoder.Write(stCode)
+                Dim g As Graphics = Graphics.FromImage(BimMap)
+                BimMap.Save(directory & "\" & stCode & ".png")
+                For Each ImageFile As String In IO.Directory.GetFiles(directory, "*.png", IO.SearchOption.AllDirectories)
+                    Dim ImgName As String = IO.Path.GetFileName(ImageFile)
+                    If ImgName = stCode & ".png" Then
+                        System.IO.File.Delete(filename)
+                    End If
+                Next
+            End If
+        Next
+    End Sub
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles Me.Shown
+        Application.DoEvents()
+        System.Threading.Thread.Sleep(5000)
+        Me.WindowState = FormWindowState.Minimized
+        Me.Hide()
+    End Sub
+    Private Sub NotifyIcon1_MouseClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseClick
+        Me.Show()
+        Me.WindowState = FormWindowState.Normal
+        Timer2.Interval = 10000
+        Timer2.Enabled = True
+        With NotifyIcon1
+            .BalloonTipIcon = ToolTipIcon.Info
+            .BalloonTipText = "Show Message"
+            .BalloonTipTitle = "Program running"
+            .ShowBalloonTip(100)
+        End With
+    End Sub
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Me.Hide()
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+   
+End Class
